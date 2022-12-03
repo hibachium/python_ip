ed3a089f4e14092d78d32e3a3a10a35d70cdba09
hibachium <aochin.ps.ai.0426@gmail.com>
2022-12-04 05:20:44 +0900

    subnetmask,mac,ipv4出力

--- source/ip.py
+++ source/ip.py
@@ -1,11 +1,26 @@
+import ipaddress
+import subprocess
+
 import netifaces
 
-interface = netifaces.ifaddresses("eth0")[netifaces.AF_LINK]
-ipaddress = netifaces.ifaddresses("eth0")[netifaces.AF_INET]
+eth0_if = netifaces.ifaddresses("eth0")[netifaces.AF_LINK]
+eth0_ip = netifaces.ifaddresses("eth0")[netifaces.AF_INET]
 
-mac = interface[0].get("addr")
-ipv4 = ipaddress[0].get("addr")
-netmask = ipaddress[0].get("netmask")
-print(mac)
-print(ipv4)
-print(netmask)
+mac = eth0_if[0].get("addr")
+ipv4 = eth0_ip[0].get("addr")
+netmask = eth0_ip[0].get("netmask")
+# print(mac)
+# print(ipv4)
+# print(netmask)
+ip = ipaddress.IPv4Network(ipv4 + "/" + netmask, strict=False)
+# print(ip)
+segment = []
+
+from ping3 import ping, verbose_ping
+
+print(ping("8.8.8.8"))
+
+# for addr in ipaddress.IPv4Network(ip, strict=False):
+#     str_addr = str(addr)
+#     segment.append(str_addr)
+#     subprocess.run(["ping", str_addr, "-c 1", "-t 1", "-i 0.5"])
